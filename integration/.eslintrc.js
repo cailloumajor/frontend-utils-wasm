13573f74177cc5d805eeead547da281bc41c1a1e
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-12-16 08:20:09 +0000

    chore: add eslint

---
+++ integration/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [
+    {
+      files: ["**/*.cy.{jx,jsx,ts,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "never"],
+  },
+}
