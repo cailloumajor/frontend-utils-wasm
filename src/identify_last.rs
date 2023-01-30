74b2151ab60e61d4d5b31e62afc99ab029fe9e87
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-01-30 15:18:01 +0000

    fix: wrongly reverted logic of identify_last iterator
    Was causing record to not be deduplicated.

--- src/identify_last.rs
+++ src/identify_last.rs
@@ -24,6 +24,21 @@ where
     type Item = (I::Item, bool);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|e| (e, self.0.peek().is_some()))
+        self.0.next().map(|e| (e, self.0.peek().is_none()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn identify_last() {
+        let mut iterator = [1, 2, 3, 4].into_iter().identify_last();
+        assert_eq!(iterator.next(), Some((1, false)));
+        assert_eq!(iterator.next(), Some((2, false)));
+        assert_eq!(iterator.next(), Some((3, false)));
+        assert_eq!(iterator.next(), Some((4, true)));
+        assert_eq!(iterator.next(), None);
     }
 }
