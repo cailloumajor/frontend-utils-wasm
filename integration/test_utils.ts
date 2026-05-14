a55f2a12302b85ac177301d0026c8949f1a23e60
Arnaud Rocher <arnaud.roche3@gmail.com>
2026-05-14 18:41:08 +0200

    chore(test): replace astral with puppeteer

--- integration/test_utils.ts
+++ integration/test_utils.ts
@@ -1,79 +1,21 @@
-import { launch, type Page } from "@astral/astral"
 import * as path from "@std/path"
+import puppeteer from "puppeteer"
 
 const size = { width: 920, height: 400 }
 
-const fps = 30
+const isInCI = ["CI", "CONTINUOUS_INTEGRATION"].some((key) => Deno.env.has(key))
 
-const ffmpegArgs = [
-  ["-loglevel", "warning"],
-  // Read images from standard input.
-  ["-f", "image2pipe", "-r", `${fps}`, "-i", "pipe:0"],
-  // No audio
-  ["-an"],
-  // Use libvpx-vp9 video encoder.
-  ["-c:v", "libvpx-vp9", "-crf", "32", "-b:v", "0", "-deadline", "realtime"],
-  // Sets the output format.
-  ["-f", "webm"],
-  // Slow down the video.
-  ["-vf", "setpts=10.0*PTS"],
-]
+let browser: puppeteer.Browser
 
-async function withRecording(
-  videoPath: string,
-  page: Page,
-  fn: () => void | Promise<void>,
-): Promise<void> {
-  const frameInterval = 1000 / fps
-
-  const ffmpegCommand = new Deno.Command("ffmpeg", {
-    // Use `-y` to overwrite output file.
-    args: [...ffmpegArgs.flat(), "-y", videoPath],
-    stdin: "piped",
+export async function startBrowser() {
+  browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: isInCI,
   })
-  await using ffmpegProcess = ffmpegCommand.spawn()
-  const toFFmpeg = ffmpegProcess.stdin.getWriter()
+}
 
-  let errorDuringInterval: Error | null = null
-
-  const timer = setInterval(
-    async () => {
-      try {
-        const png = await page.screenshot({ optimizeForSpeed: true })
-        await toFFmpeg.ready
-        await toFFmpeg.write(png)
-      } catch (err) {
-        clearInterval(timer)
-        errorDuringInterval = err as Error
-      }
-    },
-    frameInterval,
-  )
-
-  await fn()
-
-  // Wait some frames to allow recording the final status.
-  await new Promise<void>((resolve) =>
-    setTimeout(
-      () => {
-        clearInterval(timer)
-        resolve()
-      },
-      8 * frameInterval,
-    )
-  )
-
-  await toFFmpeg.ready
-  await toFFmpeg.close()
-
-  if (errorDuringInterval != null) {
-    throw errorDuringInterval
-  }
-
-  const status = await ffmpegProcess.status
-  if (!status.success) {
-    throw new Error("FFmpeg process encountered an error")
-  }
+export async function stopBrowser() {
+  await browser.close()
 }
 
 export function testId(t: Deno.TestContext): string {
@@ -83,63 +25,30 @@ export function testId(t: Deno.TestContext): string {
 export async function withBackendAndBrowser(
   t: Deno.TestContext,
   handler: Deno.ServeHandler,
-  fn: (page: Page, addr: string) => void | Promise<void>,
+  fn: (page: puppeteer.Page, addr: string) => void | Promise<void>,
 ): Promise<void> {
-  const noHeadless = Deno.args.includes("--no-headless")
-
   await using server = Deno.serve({
     hostname: "127.0.0.1",
     port: 0,
     onListen: () => {}, // Don't spam terminal with "Listening on..."
   }, handler)
 
-  await using browser = await launch({
-    args: [
-      "--no-sandbox",
-    ],
-    headless: !noHeadless,
-  })
+  const page = await browser.newPage()
 
-  await using page = await browser.newPage()
-
-  await page.setViewportSize(size)
+  await page.setViewport(size)
 
   const addr = `http://${server.addr.hostname}:${server.addr.port}`
 
-  if (noHeadless) {
+  if (!isInCI) {
     await fn(page, addr)
   } else {
-    const videosDir = path.join(import.meta.dirname!, "videos")
-    const videoFile = path.join(videosDir, `${testId(t)}.webm`)
-    await withRecording(videoFile, page, () => fn(page, addr))
-  }
-}
-
-export async function interceptResponse(
-  page: Page,
-  urlPattern: string,
-  body: string,
-): Promise<void> {
-  const celestial = page.unsafelyGetCelestialBindings()
-  await celestial.Fetch.enable({
-    patterns: [
-      {
-        urlPattern,
-        requestStage: "Response",
-      },
-    ],
-  })
-  celestial.addEventListener("Fetch.requestPaused", async (event) => {
-    const { requestId, responseStatusCode, responseHeaders, responseStatusText } = event.detail
-    if (responseStatusCode == null || responseHeaders == null || responseStatusText == null) {
-      throw new Error("bad Fetch.requestPaused event")
-    }
-    await celestial.Fetch.fulfillRequest({
-      requestId,
-      responseCode: responseStatusCode,
-      responseHeaders,
-      responsePhrase: responseStatusText,
-      body,
+    const videoFile = path.join(import.meta.dirname!, "videos", testId(t))
+    const recorder = await page.screencast({
+      path: `${videoFile}.webm`,
     })
-  })
+    await fn(page, addr)
+    await recorder.stop()
+  }
+
+  await page.close()
 }
